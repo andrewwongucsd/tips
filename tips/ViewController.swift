e8f7006a33d3177e01bfc9760d46822cc1704ae0
Andrew <naivedevelopers@gmail.com>
2015-01-01 02:15:46 -0800

    Revert 10e54ca..4bf8af7
    This rolls back to commit 10e54cab392e2035c5226160b800dfe989ac3dc8.

--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -10,9 +10,6 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var personLabel: UILabel!
-    @IBOutlet weak var slider: UISlider!
-    @IBOutlet weak var perPersonLabel: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var billField: UITextField!
@@ -21,119 +18,28 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         tipLabel.text = "$0.00"
         totalLabel.text = "$0.00"
-        perPersonLabel.text = "$0.00"
-        personLabel.text = "5"
-        var defaults = NSUserDefaults.standardUserDefaults()
-        
-        if(defaults.integerForKey("used") == 0){
-            tipControl.setTitle("15%", forSegmentAtIndex: 0)
-            tipControl.setTitle("18%", forSegmentAtIndex: 1)
-            tipControl.setTitle("20%", forSegmentAtIndex: 2)
-            defaults.setInteger(15, forKey: "left")
-            defaults.setInteger(18, forKey: "middle")
-            defaults.setInteger(20, forKey: "right")
-            defaults.synchronize()
-            defaults.setInteger(1, forKey: "used")
-        }else{
-            
-            calculate()
-        }
-        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         
     }
-    
-    func calculate(){
-        var defaults = NSUserDefaults.standardUserDefaults()
-        var leftValue = defaults.integerForKey("left")
-        var middleValue = defaults.integerForKey("middle")
-        var rightValue = defaults.integerForKey("right")
-        tipControl.setTitle(String(format: "%d%%",leftValue), forSegmentAtIndex: 0)
-        tipControl.setTitle(String(format: "%d%%",middleValue), forSegmentAtIndex: 1)
-        tipControl.setTitle(String(format: "%d%%",rightValue), forSegmentAtIndex: 2)
-        let red = defaults.integerForKey("red")
-        let green = defaults.integerForKey("green")
-        let blue = defaults.integerForKey("blue")
-        println("\(red) \(blue) \(green) in Calculate")
-        var color = UIColor(red: CGFloat(red)/255.0, green: CGFloat(green)/255.0, blue: CGFloat(blue)/255.0, alpha: CGFloat(1.0))
-        self.view.backgroundColor = color
-        if(red < 63 || green < 63){
-            let white = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
-
-        }
-
-
-        
-        if(billField.text.isEmpty != true){
-            var defaults = NSUserDefaults.standardUserDefaults()
-            var left = defaults.integerForKey("left")
-            var middle = defaults.integerForKey("middle")
-            var right = defaults.integerForKey("right")
-            var tipPercentages = [left,middle, right]
-            var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
-            var tipConverted = Double(tipPercentage)
-            println("\(left) \(middle) \(right) \(tipPercentage) \(tipConverted)")
-            var billAmount = (billField.text as NSString).doubleValue
-            var numberOfPerson = Int(slider.value)
-            var tip = billAmount * tipConverted * 0.001
-            var total = billAmount + tip
-            var perPerson = total/Double(numberOfPerson)
-            tipLabel.text = "\(tip)"
-            totalLabel.text = "\(total)"
-            perPersonLabel.text = "\(perPerson)"
-            
-            var formatter = NSNumberFormatter()
-            formatter.numberStyle = .CurrencyStyle
-            formatter.locale = NSLocale.currentLocale()
-            //tipLabel.text = String(format: "$%.2f", tip)
-            tipLabel.text = formatter.stringFromNumber(tip)
-            //totalLabel.text = String(format: "$%.2f", total)
-            totalLabel.text = formatter.stringFromNumber(total)
-            //perPersonLabel.text = String(format: "$%.2f", perPerson)
-            perPersonLabel.text = formatter.stringFromNumber(perPerson)
-        }
-    }
-    
 
     @IBAction func onEditingChanged(sender: AnyObject) {
-
-        calculate()
-
+        var tipPercentages = [0.15,0.2,0.22]
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        var billAmount = (billField.text as NSString).doubleValue
+        var tip = billAmount * tipPercentage
+        var total = billAmount + tip
+        tipLabel.text = "\(tip)"
+        totalLabel.text = "\(total)"
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
     }
 
-    @IBAction func sliderValueChanged(sender: UISlider) {
-        var currentValue = Int(sender.value)
-        personLabel.text = "\(currentValue)"
-    }
     @IBAction func onTap(sender: AnyObject) {
         view.endEditing(true)
-    }
-    
-    override func viewWillAppear(animated: Bool) {
-        super.viewWillAppear(animated)
-        println("view will appear")
-
-    }
-    
-    override func viewDidAppear(animated: Bool) {
-        super.viewDidAppear(animated)
-        println("view did appear")
-
-        
-        calculate()
-    }
-    
-    override func viewWillDisappear(animated: Bool) {
-        super.viewWillDisappear(animated)
-        println("view will disappear")
-    }
-    
-    override func viewDidDisappear(animated: Bool) {
-        super.viewDidDisappear(animated)
-        println("view did disappear")
     }
 }
 
